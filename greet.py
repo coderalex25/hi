96da798bd8f483b0a3e173edb4c4de86c5b9a9e0
Alex <coderalex25@gmail.com>
2025-05-13 20:55:03 +0000

    finsh

--- greet.py
+++ greet.py
@@ -1,5 +1,20 @@
 name = input ("what is your name ")
+
 color = input ("what is your favorite color ")
+
 print ("Hello "+ name +" your favorite color is "+color)
+
 print ("what is your favorite football team")
+
 team = input ("your favorite football team is ")
+print ("Lets go "+ team)
+
+food = input ("your favorite food is ")    
+print ("That was good i love "+ food ) 
+
+move = input ("your favorite move is")
+print ("that was great i love "+ move)
+
+song = input ("your favorite song is")
+print ("that was awesome i like "+ song)
+
