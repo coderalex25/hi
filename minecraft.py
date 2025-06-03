b3c40811e5ab3743875fd96488178fad5b6cc9b9
Alex <coderalex25@gmail.com>
2025-06-03 20:53:14 +0000

    mincraft qize

---
+++ minecraft.py
@@ -0,0 +1,43 @@
+print("how many harts does a player haves? ")
+score=0
+print("A: 5")
+print("B: 8")
+print("C: 2")
+print("D: 10")
+answer=input("what is your answer? ")
+if answer=="D":
+    print("correct")
+    score=score+5
+    print(score)
+else:
+    print("wrong")
+    score=score-5
+    print(score)
+print("who do you have to kill to beat the game? ")
+print("A: irongolem")
+print("B: warden")
+print("C: enderdragon")
+print("D: wither")
+answer=input("what is your answer? ")
+if answer=="C":
+    print("correct")
+    score=score+5
+    print(score)
+else:
+    print("wrong")
+    score=score-5
+    print(score)
+print("how many blocks are in a stack? ")
+print("A: 16")
+print("B: 64")
+print("C: 54")
+print("D: 84")
+answer=input("what is your answer? ")
+if answer=="B":
+    print("correct")
+    score=score+5
+    print(score)
+else:
+    print("wrong")
+    score=score-5
+    print(score)
