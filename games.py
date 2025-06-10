b7e9d6841ebe182f3460b407ae32626e6b189fd1
Alex <coderalex25@gmail.com>
2025-06-10 20:47:41 +0000

    games.py

--- games.py
+++ games.py
@@ -1,14 +1,46 @@
 from random import randint
-player =input("what is your move(rock paper and scissors)? ")
-computer = randint(1, 3)
+score=0
+alex = True
+while(alex):
 
-if player == "rock" and computer == 1:
-    print("its a tie!")
-elif player == "paper" and computer == 2:
-    print("its a tie!")
-elif player == "scissors" and computer == 3:
-    print("its a tie!")
+    player =input("what is your move(rock paper and scissors)? ")
+    computer = randint(1, 3)
 
-elif player == "rock" and computer == 3:
-    print("player wins! ")
-elif player == 
+    if computer == 1:
+        print("computer played rock")
+    if computer == 2:
+        print("computer played paper")
+    if computer == 3:
+        print("computer played scissors")
+
+    if player == "rock" and computer == 1:
+        print("its a tie!")
+    elif player == "paper" and computer == 2:
+        print("its a tie!")
+    elif player == "scissors" and computer == 3:
+        print("its a tie!")
+
+    elif player == "rock" and computer == 3:
+        print("player wins! ")
+        score=score+1
+    elif player == "paper" and computer == 1:
+        print("player wins")
+        score=score+1
+    elif player == "scissors" and computer == 2:
+        print("player wins")
+        score=score+1
+            
+    elif player == "rock" and computer == 2:
+        print("computer wins! ")
+    elif player == "paper" and computer == 3:
+        print("computer wins")
+    elif player == "scissors" and computer == 1:
+        print("computer wins")
+    
+    print("your score is ")
+    print (score)
+
+    bob =input ("do you whent to continue ")
+    if bob =="no":
+        alex = False
+    
